c9bcc9f91f4b9579c2ec3840df3046f0d03a938a
Cirmaci <81503506+obvCirmaci@users.noreply.github.com>
2021-07-28 07:44:05 +0300

    Update install.bat

--- src river client installer/install.bat
+++ src river client installer/install.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=1OFGyAQz5XVe0EvCA6u4R4tV8ahVYNV3d', 'River-Client.jar')"
-powershell -Command "Invoke-WebRequest https://drive.google.com/uc?export=download&id=1OFGyAQz5XVe0EvCA6u4R4tV8ahVYNV3d -OutFile River Client.jar"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('client download', 'River-Client.jar')"
+powershell -Command "Invoke-WebRequest client download -OutFile River Client.jar"
 move "River-Client.jar" %appdata%\.minecraft\versions\River-Client
 exit
